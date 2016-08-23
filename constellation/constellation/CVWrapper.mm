3580d65b7cf9f6907710f9da95cb64fec639897d
stanchiang <stanchiang23@gmail.com>
2016-08-23 09:26:20 -0700

    added book code; still need to double check how its working so i can draw it

--- constellation/constellation/CVWrapper.mm
+++ constellation/constellation/CVWrapper.mm
@@ -8,9 +8,11 @@
 
 #import <UIKit/UIKit.h>
 #import "CVWrapper.h"
-#import "detector.hpp"
+#import "ARPipeline.hpp"
+
 #import <opencv2/imgproc/imgproc.hpp>
 #import <opencv2/highgui/cap_ios.h>
+
 #include <iostream>
 @interface CVWrapper()<CvVideoCameraDelegate> {
     
@@ -22,6 +24,7 @@
 - (void) startCamera:(UIView *) view {
     _videoSource = [[CvVideoCamera alloc] initWithParentView:view];
     _videoSource.defaultAVCaptureDevicePosition = AVCaptureDevicePositionBack;
+    _videoSource.defaultAVCaptureSessionPreset = AVCaptureSessionPreset1280x720;
     _videoSource.delegate = self;
     _videoSource.rotateVideo = true;
 //    _videoSource.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
@@ -30,7 +33,39 @@
 }
 
 - (void)processImage:(cv::Mat&)image {
-    cv::circle(image, cv::Point(50, 50), 3, cv::Scalar(0,250,0), -1 );
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"pic1" ofType:@"bmp"];
+    const char * cpath = [path cStringUsingEncoding:NSUTF8StringEncoding];
+    cv::Mat patternImage = cv::imread( cpath, CV_LOAD_IMAGE_UNCHANGED );
+    
+    CameraCalibration calibration(1136, 320, 1041, 240);
+    ARPipeline pipeline(patternImage, calibration);
+    
+    cv::Size frameSize(image.cols, image.rows);
+    
+    processFrame(image, pipeline);
 }
 
+bool processFrame(const cv::Mat& cameraFrame, ARPipeline& pipeline) {
+    // Clone image used for background (we will draw overlay on it)
+    cv::Mat img = cameraFrame.clone();
+    
+    if (pipeline.m_patternDetector.enableHomographyRefinement) {
+        printf("Pose refinement: On");
+    } else {
+        printf("Pose refinement: Off");
+    }
+    
+    printf("RANSAC threshold: %f",pipeline.m_patternDetector.homographyReprojectionThreshold);
+    
+    // Find a pattern
+    pipeline.processFrame(cameraFrame);
+    
+    // Update a pattern pose
+    pipeline.getPatternLocation();
+    
+    // Request redraw of the window
+    cv::circle(img, cv::Point(50, 50), 3, cv::Scalar(0,250,0), -1 );
+    
+    return true;
+}
 @end
